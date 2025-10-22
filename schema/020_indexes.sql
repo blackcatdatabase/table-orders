1204f671d2d20b40c894d8bcaa6f7487cfd1cd37
Tolben <vitek.pasek@gmail.com>
2025-10-22 20:58:57 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/020_indexes.sql
+++ schema/020_indexes.sql
@@ -1,3 +1,3 @@
--- Auto-generated from schema-map.psd1 (map@1e83bb6)
+-- Auto-generated from schema-map.psd1 (map@6cefe8e)
 -- table: orders
 CREATE INDEX idx_orders_created_at ON orders (created_at);
